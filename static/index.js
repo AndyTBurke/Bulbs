bf8745d99ab18221503b2445db1d1d40c973cd1f
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2023-06-01 19:56:58 -0500

    copy btn displays copied! when clicked

--- static/index.js
+++ static/index.js
@@ -2,6 +2,6 @@ document.getElementById('copy-btn').addEventListener('click', () => copy())
 
 function copy() {
     const binary = document.getElementById('binary-message').innerText;
-
     navigator.clipboard.writeText(binary)
+    document.getElementById('copy-btn').innerText = "COPIED!"
 }
